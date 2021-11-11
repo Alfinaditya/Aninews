539ea0acd40709edede11c036816ea815012f4b5
Alfinadtiya <alfinaditia02@gmail.com>
2021-11-11 13:02:20 +0700

    Fix skeleton loading

--- src/parts/navbar/components/BottomNavbar.tsx
+++ src/parts/navbar/components/BottomNavbar.tsx
@@ -3,7 +3,7 @@ import { NavLink } from 'react-router-dom';
 
 const BottomNavbar = () => {
 	return (
-		<nav className='lg:hidden bg-main w-full fixed bottom-0 h-16 flex items-center'>
+		<nav className='lg:hidden bg-main w-full fixed z-10 bottom-0 h-16 flex items-center'>
 			<div className='flex justify-between w-11/12 m-auto'>
 				{navLinks.map((navLink, i) => (
 					<NavLink
